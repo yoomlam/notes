ed6bb41e1ec91e5627775b022515c8d2629be787
yoomlam <yoom@navapbc.com>
2023-10-17 11:14:39 -0500

    add python_notebook

---
+++ python_notebook/config.py
@@ -0,0 +1,8 @@
+
+# https://stackoverflow.com/questions/42163470/how-to-execute-a-py-file-from-a-ipynb-file-on-the-jupyter-notebook
+# %run -i 'setup.py'
+
+# https://stackoverflow.com/questions/43865291/import-function-from-a-file-in-the-same-folder?rq=1
+# import setup as Setup
+
+my_secret="hi3"
